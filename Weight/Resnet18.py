357efc8d6d63e525ba87a98b432c2d20eb75c198
wangb20 <s89745380@gmail.com>
2024-11-21 21:09:05 +0000

    Update Resnet18.py

--- Weight/Resnet18.py
+++ Weight/Resnet18.py
@@ -34,11 +34,11 @@ class Cutout(object):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
-        transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+        transforms.RandomCrop(32, padding=4),  
+        transforms.RandomHorizontalFlip(),  
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        Cutout(n_holes=1, length=16)  # R,G,B每层的归一化用到的均值和方差
+        Cutout(n_holes=1, length=16)  
     ])
  
 transform_test = transforms.Compose([
@@ -113,7 +113,6 @@ for idx in indices:
 weight_changes = [[] for _ in indices]
 weight_changes1 = [[] for _ in indices]
 
-# 添加计算平均信息熵的类
 class LayerEntropyHook:
     def __init__(self, module, layer_name):
         
@@ -185,10 +184,8 @@ for epoch in range(250):  # loop over the dataset
     train_loss = running_loss / len(trainloader)
     train_losses.append(train_loss)
     
-    # 记录信息熵到WandB
     wandb.log({'epoch': epoch, 'accuracy': train_acc, 'loss': train_loss})
     
-    # 记录各层信息熵到WandB
     for hook in entropy_hooks:
         wandb.log({f'{hook.layer_name}_entropy': torch.mean(torch.tensor(hook.entropies))})
         
